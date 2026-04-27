464473747eb6250ca7c833d620ae54f5bcf74e87
isaia951207 <125284308+isaia951207@users.noreply.github.com>
2026-04-27 11:52:05 +0800

    week10

---
+++ week10/week10-4.cpp
@@ -0,0 +1,16 @@
+///week10-4.cpp 交換2瓶水
+#include <stdio.h>
+int main()
+{
+    /// 黑色的酒 vs. 100%白開水
+    int a = 9, b = 100; /// 一開始
+
+    printf("a:%d b:%d\n", a, b); /// 印出來
+
+    /// 交換一下 兩瓶做交換
+    int temp = a; /// 臨時拿一個瓶子
+    a = b;
+    b = temp;
+
+    printf("a:%d b:%d\n", a, b); /// 印出來
+}
